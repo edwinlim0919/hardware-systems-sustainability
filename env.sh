9906b8f384a6b87d0903cf3e45738dbaeef09079
edwinlim <edwinlim@node0.grpc-hotel-py.datacenter-soc-pg0.utah.cloudlab.us>
2023-09-18 16:26:26 -0600

    guh

--- env.sh
+++ env.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# Installation script for some basic bash needs
 
 sudo apt install vim
 sudo apt install tmux
+
+CURR_DIR=$(pwd)
+cp ${CURR_DIR}/dotfiles/bashrc ~/.bashrc
+cp ${CURR_DIR}/dotfiles/bash_prompt ~/.bash_prompt
