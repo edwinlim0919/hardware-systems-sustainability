a3ae5a51eaabbe6c5d112007d69a81835ba27a01
edwinlim <edwinlim@node6.grpc-hotel-py.datacenter-soc-pg0.utah.cloudlab.us>
2023-09-19 13:21:57 -0600

    wip

--- scripts/main.py
+++ scripts/main.py
@@ -12,6 +12,7 @@ logger = logging.getLogger('grpc-hotel-ipu')
 def setup_docker_swarm(advertise_addr):
     logger.info('----------------')
     logger.info('Setting up docker swarm...')
+    logger.info('advertise-addr: ' + str())
     logger.info('----------------')
 
 if __name__ == '__main__':
@@ -21,14 +22,20 @@ if __name__ == '__main__':
     #parser.add_argument('-o', '--option', dest='option', type=str, help='Example of str arg')
     #parser.add_argument('file', metavar='file', type=str, help='Example of a positional argument')
     parser.add_argument('--setup-docker-swarm',
+                        dest='setup_docker_swarm',
                         action='store_true',
                         help='specify arg to set up docker swarm')
     parser.add_argument('--advertise-addr',
+                        dest='advertise_addr',
                         type=str,
-                        help='')
+                        help='advertised address for the docker swarm')
     args = parser.parse_args()
 
     if args.setup_docker_swarm:
-
+        if (args.advertise_addr is None or
+            not isinstance(args.advertise_addr, str) or
+            not ):
+            raise ValueError('advertise-addr IP address in string format is required for setting up a docker swarm')
+        setup_docker_swarm(args.advertise_addr)
     #logger.info('--------------')
     #logger.info('Hello World!')
