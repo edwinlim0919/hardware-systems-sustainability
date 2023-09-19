b6c332a59649f5dd5e36c0e115584f99c5762f4f
edwinlim <edwinlim@node4.grpc-hotel-py.datacenter-soc-pg0.utah.cloudlab.us>
2023-09-18 18:19:07 -0600

    e

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+curl -fsSL https://get.docker.com -o scripts/get-docker.sh
+sudo sh scripts/get-docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-wget https://bootstrap.pypa.io/get-pip.py
-python3 ./get-pip.py
+wget https://bootstrap.pypa.io/get-pip.py scripts/get-pip.py
+python3 ./scripts/get-pip.py
 setenv PATH "{$PATH}:/users/`whoami`/.local/bin"
 pip3 install asyncio aiohttp
 
