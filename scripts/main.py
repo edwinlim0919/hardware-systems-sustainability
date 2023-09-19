91cf16017c8ae5534d0c91c0e2c227b510ea3cae
edwinlim <edwinlim@node6.grpc-hotel-py.datacenter-soc-pg0.utah.cloudlab.us>
2023-09-19 11:06:09 -0600

    skeleton docker setup

--- scripts/main.py
+++ scripts/main.py
@@ -1,25 +1,26 @@
 import sys
 import os
 import argparse
-import getpass
 import logging
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-logger = logging.getLogger('upload_bom')
+logger = logging.getLogger('grpc-hotel-ipu')
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-u', '--user', dest='email', type=str, help='Username/Email used for login')
-    parser.add_argument('-p', '--production', dest='production', action='store_true', help='Example of boolean arg')
-    parser.add_argument('-o', '--option', dest='option', type=str, help='Example of str arg')
-
-    parser.add_argument('file', metavar='file', type=str, help='Example of a positional argument')
-
+    #parser.add_argument('-u', '--user', dest='email', type=str, help='Username/Email used for login')
+    #parser.add_argument('-p', '--production', dest='production', action='store_true', help='Example of boolean arg')
+    #parser.add_argument('-o', '--option', dest='option', type=str, help='Example of str arg')
+    #parser.add_argument('file', metavar='file', type=str, help='Example of a positional argument')
+    parser.add_argument('--setup-docker-swarm',
+                        action='store_true',
+                        help='specify arg to set up docker swarm')
     args = parser.parse_args()
-    logger.info('--------------')
 
-    # Never ask for a password in command-line. Manually ask for it here
-    password = getpass.getpass()
-    
-    logger.info('Hello World!')
+    if args.setup_docker_swarm:
+        logger.info('----------------')
+        logger.info('setting up docker swarm')
+        logger.info('----------------')
+
+    #logger.info('--------------')
+    #logger.info('Hello World!')
