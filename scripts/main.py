f66b8f7193f59b70ffc583eb559a1f1d715c1856
edwinlim <edwinlim@node6.grpc-hotel-py.datacenter-soc-pg0.utah.cloudlab.us>
2023-09-22 21:03:55 -0600

    slepey

--- scripts/main.py
+++ scripts/main.py
@@ -11,24 +11,35 @@ import utils
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger('grpc-hotel-ipu')
 
-def setup_docker_swarm():
+def setup_docker_swarm(args):
     advertise_addr = utils.parse_ifconfig(logger)
-    docker_swarm_init_cmd = "sudo docker swarm init " + \
-                            "--advertise-addr " + advertise_addr
+    docker_swarm_init_str = 'sudo docker swarm init ' + \
+                            '--advertise-addr {0}'
+    docker_swarm_init_cmd = docker_swarm_init_str.format(advertise_addr)
 
-    logger.info('----------------')
-    logger.info('Setting up docker swarm...')
-    logger.info('advertise-addr: ' + advertise_addr)
-    logger.info('init-cmd: ' + docker_swarm_init_cmd)
-    logger.info('Initializing docker swarm...')
-    subprocess.Popen(docker_swarm_init_cmd.split()).wait()
-    logger.info('Starting local registry on this node...')
+    docker_service_create_str = 'sudo docker service create ' + \
+                                '--name registry ' + \
+                                '--publish published={0},target={1} registry:{2}'
+    docker_service_create_cmd = docker_service_create_str.format(args.published,
+                                                                 args.target,
+                                                                 args.registry)
 
-    logger.info('Set up docker swarm successfully.')
-    logger.info('----------------')
+    print(docker_swarm_init_cmd)
+    print(docker_service_create_cmd)
+
+    #logger.info('----------------')
+    #logger.info('Setting up docker swarm...')
+    #logger.info('advertise-addr: ' + advertise_addr)
+    #logger.info('init-cmd: ' + docker_swarm_init_cmd)
+    #logger.info('Initializing docker swarm...')
+    #subprocess.Popen(docker_swarm_init_cmd.split()).wait()
+    #logger.info('Starting local registry on this node...')
+
+    #logger.info('Set up docker swarm successfully.')
+    #logger.info('----------------')
 
 def leave_docker_swarm(is_manager):
-    docker_swarm_leave_cmd = "sudo docker swarm leave --force"
+    docker_swarm_leave_cmd = 'sudo docker swarm leave --force'
 
     logger.info('----------------')
     logger.info('Leaving docker swarm...')
@@ -48,7 +59,15 @@ if __name__ == '__main__':
     parser.add_argument('--published',
                         dest='published',
                         type=int,
-                        help='specify the published arg for creating docker registry service')
+                        help='specify the published value for creating docker registry service')
+    parser.add_argument('--target',
+                        dest='target',
+                        type=int,
+                        help='specify the target value for creating docker registry service')
+    parser.add_argument('--registry',
+                        dest='registry',
+                        type=int,
+                        help='specify the registry value for creating docker registry service')
     parser.add_argument('--leave-docker-swarm',
                         dest='leave_docker_swarm',
                         action='store_true',
@@ -60,8 +79,12 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.setup_docker_swarm:
-        #setup_docker_swarm()
         if args.published is None:
-            raise ValueError('published arg should be specified for creating docker registry service')
+            raise ValueError('published value should be specified for creating docker registry service')
+        if args.target is None:
+            raise ValueError('target value should be specified for creating docker registry service')
+        if args.registry is None:
+            raise ValueError('registry value should be specified for creating docker registry service')
+        setup_docker_swarm(args)
     if args.leave_docker_swarm:
         leave_docker_swarm(args.is_manager)
