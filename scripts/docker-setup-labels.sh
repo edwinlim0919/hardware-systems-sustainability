fa48dfc663969430b9c2c85c162168ec06dbf11b
edwinlim <edwinlim@node0.grpc-hotel-py.datacenter-soc-pg0.utah.cloudlab.us>
2023-10-10 21:24:56 -0600

    missing script from sriram's repo

--- scripts/docker-setup-labels.sh
+++
@@ -1,7 +0,0 @@
-all_nodes=$(sudo docker node ls)
-
-while IFS= read -r line; do
-	node_name=$(echo "$line" | awk 'BEGIN {FS=" "}; {print $2}')
- 	node_label=$(echo "$node_name" | awk 'BEGIN {FS="."}; {print $1}')
-	sudo docker node update --label-add "$node_label"=true "$node_name"
-done <<< "$all_nodes"
