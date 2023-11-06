ef6bb01e909b041c23c79b6455afb1efef581816
edwinlim <edwinlim@node0.grpc-c6320.datacenter-soc-pg0.clemson.cloudlab.us>
2023-11-06 17:48:26 -0500

    e

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -11,10 +11,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 #import pandas as pd
 
-plotting = False
+#plotting = False
+plotting = True
+#profiling = True
 
 if plotting:
-    filename = '../results/hotelreservation_grpc-50-50-20s-100-200-1.2'
+    #filename = '../results/hotelreservation_grpc-50-50-20s-100-200-1.2'
+    filename = '../results/hotelreservation_grpc-50-50-2m-100-3000-1.1'
     file = open(filename, 'r')
     lines = file.readlines()
     
@@ -28,5 +31,7 @@ if plotting:
     
     plt.plot(rps_list, lat_list)
     plt.xlabel("Requests Per Second")
-    plt.ylabel("Mean Latency")
+    plt.ylabel("Mean Latency (ms)")
     plt.savefig(filename + '.png', dpi='figure', format=None)
+
+#if profiling:
