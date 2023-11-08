e68f75991ef1c116e3ce14716f5c1b8f2b5918ef
edwinlim <edwinlim@node0.grpc-c6320.datacenter-soc-pg0.clemson.cloudlab.us>
2023-11-08 10:03:28 -0500

    e

--- scripts/run-perf.sh
+++ scripts/run-perf.sh
@@ -1,22 +1,24 @@
-container_ids=()
+sudo perf record -F 250 -e $1 --call-graph lbr -p $2 sleep 10
 
-while read -r line
-do
-	id=${line%% *}
-	container_ids+=("${id}")
-done < <(sudo docker ps | grep $1)
-
-all_ps=""
-for id in ${container_ids[@]}
-do
-	out=$(sudo docker top $id | awk '{print $2}')
-	for each_pid in ${out}; do
-		if [ "$each_pid" != "PID" ]; then
-			all_ps="${each_pid},${all_ps}"
-		fi
-	done
-done
-
-echo "Running perf record for the processes $all_ps"
-
-sudo perf record -e cpu-cycles:ppp --call-graph lbr -p ${all_ps}
+#container_ids=()
+#
+#while read -r line
+#do
+#	id=${line%% *}
+#	container_ids+=("${id}")
+#done < <(sudo docker ps | grep $1)
+#
+#all_ps=""
+#for id in ${container_ids[@]}
+#do
+#	out=$(sudo docker top $id | awk '{print $2}')
+#	for each_pid in ${out}; do
+#		if [ "$each_pid" != "PID" ]; then
+#			all_ps="${each_pid},${all_ps}"
+#		fi
+#	done
+#done
+#
+#echo "Running perf record for the processes $all_ps"
+#
+#sudo perf record -e cpu-cycles:ppp --call-graph lbr -p ${all_ps}
