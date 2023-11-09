056be331b9583eced9a789617258b89ccbe88007
edwinlim <edwinlim@node0.hmm.datacenter-soc-pg0.clemson.cloudlab.us>
2023-11-09 16:37:24 -0500

    e

---
+++ scripts/bucketize_all.sh
@@ -0,0 +1,23 @@
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/branches_branch-misses_perf.hist.0 > /dev/shm/grpc-hotel-ipu/results/cycles_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/branches_branch-misses_perf.hist.1 > /dev/shm/grpc-hotel-ipu/results/instructions_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/branches_branch-misses_perf.hist.2 > /dev/shm/grpc-hotel-ipu/results/branches_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/branches_branch-misses_perf.hist.3 > /dev/shm/grpc-hotel-ipu/results/branch-misses_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/dTLB-loads_dTLB-load-misses_perf.hist.2 > /dev/shm/grpc-hotel-ipu/results/dTLB-loads_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/dTLB-loads_dTLB-load-misses_perf.hist.3 > /dev/shm/grpc-hotel-ipu/results/dTLB-load-misses_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/dTLB-stores_dTLB-store-misses_perf.hist.2 > /dev/shm/grpc-hotel-ipu/results/dTLB-stores_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/dTLB-stores_dTLB-store-misses_perf.hist.3 > /dev/shm/grpc-hotel-ipu/results/dTLB-store-misses_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/iTLB-loads_iTLB-load-misses_perf.hist.2 > /dev/shm/grpc-hotel-ipu/results/iTLB-loads_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/iTLB-loads_iTLB-load-misses_perf.hist.3 > /dev/shm/grpc-hotel-ipu/results/iTLB-load-misses_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/L1-dcache-loads_L1-dcache-load-misses_perf.hist.2 > /dev/shm/grpc-hotel-ipu/results/L1-dcache-loads_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/L1-dcache-loads_L1-dcache-load-misses_perf.hist.3 > /dev/shm/grpc-hotel-ipu/results/L1-dcache-load-misses_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/L1-icache-load-misses_L1-dcache-stores_perf.hist.2 > /dev/shm/grpc-hotel-ipu/results/L1-icache-load-misses_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/L1-icache-load-misses_L1-dcache-stores_perf.hist.3 > /dev/shm/grpc-hotel-ipu/results/L1-dcache-stores_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/L2-loads_L2-load-misses_perf.hist.2 > /dev/shm/grpc-hotel-ipu/results/L2-loads_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/L2-loads_L2-load-misses_perf.hist.3 > /dev/shm/grpc-hotel-ipu/results/L2-load-misses_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/L2-stores_L2-store-misses_perf.hist.2 > /dev/shm/grpc-hotel-ipu/results/L2-stores_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/L2-stores_L2-store-misses_perf.hist.3 > /dev/shm/grpc-hotel-ipu/results/L2-store-misses_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/LLC-loads_LLC-load-misses_perf.hist.2 > /dev/shm/grpc-hotel-ipu/results/LLC-loads_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/LLC-loads_LLC-load-misses_perf.hist.3 > /dev/shm/grpc-hotel-ipu/results/LLC-load-misses_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/LLC-stores_LLC-store-misses_perf.hist.2 > /dev/shm/grpc-hotel-ipu/results/LLC-stores_breakdown.txt
+python3 track_higher.py /dev/shm/grpc-hotel-ipu/results/c6420_perfdata/c6420_perfdata/perfdata/LLC-stores_LLC-store-misses_perf.hist.3 > /dev/shm/grpc-hotel-ipu/results/LLC-store-misses_breakdown.txt
+
