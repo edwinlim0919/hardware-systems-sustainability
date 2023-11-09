39b14901bc58d671a374d36c1fd09379497a92e9
edwinlim <edwinlim@node0.grpc-c6320.datacenter-soc-pg0.clemson.cloudlab.us>
2023-11-09 02:08:32 -0500

    e

--- scripts/latsweep_plot_scratch.py
+++ scripts/latsweep_plot_scratch.py
@@ -11,22 +11,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-filename = sys.argv[1]
+filename1 = sys.argv[1]
+filename2 = sys.argv[2]
 
-#filename_path = '../results/hotelreservation_grpc-50-50-2m-100-3000-1.1'
-filename_path = '../results/' + filename
-file = open(filename_path, 'r')
-lines = file.readlines()
+filename1_path = '../results/' + filename1
+filename2_path = '../results/' + filename2
+file1 = open(filename1_path, 'r')
+lines1 = file1.readlines()
+file2 = open(filename2_path, 'r')
+lines2 = file2.readlines()
 
-rps_list = []
-lat_list = []
-for line in lines:
+rps_list1 = []
+lat_list1 = []
+rps_list2 = []
+lat_list2 = []
+
+for line in lines1:
     newline_removed = line[:-1]
     line_split = newline_removed.split()
-    rps_list.append(int(line_split[0]))
-    lat_list.append(float(line_split[1]))
+    rps_list1.append(int(line_split[0]))
+    lat_list1.append(float(line_split[1]))
+for line in lines2:
+    newline_removed = line[:-1]
+    line_split = newline_removed.split()
+    rps_list2.append(int(line_split[0]))
+    lat_list2.append(float(line_split[1]))
 
-plt.plot(rps_list, lat_list)
+plt.plot(rps_list1, lat_list1, color='red', label='c6320')
+plt.plot(rps_list2, lat_list2, color='blue', label='c6420')
+plt.legend()
 plt.xlabel("Requests Per Second")
 plt.ylabel("Mean Latency (ms)")
-plt.savefig(filename_path + '.png', dpi='figure', format=None)
+plt.savefig('../results/bothsweeps' + '.png', dpi='figure', format=None)
