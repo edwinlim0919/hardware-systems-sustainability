8f60e130f9eb7c1db10d14926f097c377075a93e
edwinlim <edwinlim@node0.sgrfcmu.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-01 11:19:44 -0500

    cringe medium article tutorial not working

--- sustainable-deep-learning/scripts/sentiment_classification.py
+++ sustainable-deep-learning/scripts/sentiment_classification.py
@@ -1,11 +1,34 @@
-import joblib
-import s3fs
-import sklearn
+from transformers import pipeline
+#import joblib
+#import s3fs
+#import sklearn
 
-@serve.deployment(route_prefix='/sentiment', name='sentiment-deployment')
+@serve.deployment(route_prefix='/sentiment', name='sentiment')
 class SentimentDeployment:
     def __init__(self):
-        fs = s3fs.S3FileSystem(anon=True)
-    with fs.open('ray-serve-blog/unigram_vectorizer.joblib', 'rb') as f:
-        self.vectorizer = joblib.load(f)
-        f
+        self.classifier = pipeline('sentiment-analysis')
+
+    async def __call__(self, request):
+        data = await request.body()
+        [result] = self.classifier(str(data))
+        return result['label']
+
+#class SentimentDeployment:
+#    def __init__(self):
+#        fs = s3fs.S3FileSystem(anon=True)
+#        with fs.open('ray-serve-blog/unigram_vectorizer.joblib', 'rb') as f:
+#            self.vectorizer = joblib.load(f)
+#        with fs.open('ray-serve-blog/unigram_tf_idf_transformer.joblib', 'rb') as f:
+#            self.preprocessor = joblib.load(f)
+#        with fs.open('ray-serve-blog/unigram_tf_idf_classifier.joblib', 'rb') as f:
+#            self.classifier = joblib.load(f)
+#
+#    async def __call__(self, request):
+#        data = await request.body()
+#        vectorized = self.vectorizer.transform([str(data)])
+#        transformed = self.preprocessor.transform(vectorized)
+#        [result] = self.classifier.predict(transformed)
+#        if result == 1:
+#            return 'POSITIVE'
+#        else:
+#            return 'NEGATIVE'
