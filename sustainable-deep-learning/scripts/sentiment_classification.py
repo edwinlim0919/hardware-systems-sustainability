6c00e12987de2f4ffac6e490754373c337627a16
edwinlim <edwinlim@node0.sgrfcmu.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-01 10:30:02 -0500

    vim indents

---
+++ sustainable-deep-learning/scripts/sentiment_classification.py
@@ -0,0 +1,10 @@
+import joblib
+import s3fs
+import sklearn
+
+@serve.deployment(route_prefix='/sentiment', name='sentiment-deployment')
+class SentimentDeployment:
+    def __init__(self):
+        fs = s3fs.S3FileSystem(anon=True)
+	with fs.open('ray-serve-blog/unigram_vectorizer.joblib', 'rb') as f:
+
