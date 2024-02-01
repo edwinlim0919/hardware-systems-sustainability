b8242399836ec618ad011560ec2f17f94cee7b92
edwinlim <edwinlim@node0.sgrfcmu.datacenter-soc-pg0.clemson.cloudlab.us>
2024-01-31 23:57:48 -0500

    redoing setups

--- sustainable-deep-learning/setup.sh
+++ sustainable-deep-learning/setup.sh
@@ -4,3 +4,8 @@ pip install tqdm boto3 requests regex sentencepiece sacremoses
 pip install torch
 pip install huggingface_hub
 pip install transformers
+pip install "ray[serve]"
+pip install s3fs
+pip install joblib
+pip install scikit-learn
+pip install pygments
