148949768c992224ac032ee50d57d8f0591cb509
edwinlim <edwinlim@node0.sgrfcmu.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-01 17:33:15 -0500

    e

--- sustainable-deep-learning/scripts/ray_bert-base-uncased_model.py
+++ sustainable-deep-learning/scripts/ray_bert-base-uncased_model.py
@@ -1,2 +1,20 @@
 import torch
 from transformers import BertModel, BertTokenizer
+
+
+class BertModel:
+    def __init__(self):
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    def tokenize(self, text: str) -> str:
+        query_ids = self.tokenizer.encode(text, return_tensors='pt')
+        #query_tokens = self.tokenizer.convert_ids_to_tokens(query_ids)
+        #query_ids = torch.LongTensor(query_ids)
+        print('query_ids: ' + str(query_ids))
+        print('type of query_ids' + str(type(query_ids)))
+        #print('query_tokens: ' + str(query_tokens))
+        return 'e'
+
+
+bertmodel = BertModel()
+bertmodel.tokenize('granola bars')
