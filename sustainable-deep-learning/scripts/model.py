43fc0e66dbb7fa4827d0063bbe78c36bc8653599
edwinlim <edwinlim@node0.sgrfcmu.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-01 14:15:56 -0500

    cleaning and ray config directory

--- sustainable-deep-learning/scripts/model.py
+++ sustainable-deep-learning/scripts/model.py
@@ -3,9 +3,6 @@ from ray import serve
 from fastapi import FastAPI
 
 from transformers import pipeline
-#import joblib
-#import s3fs
-#import sklearn
 
 
 app = FastAPI()
@@ -24,40 +21,4 @@ class Translator:
         return translation
 
 
-#translator = Translator()
-#translation = translator.translate('Hello world!')
-#print(translation)
-
-
 translator_app = Translator.bind()
-
-
-#@serve.deployment(route_prefix='/sentiment', name='sentiment')
-#class SentimentDeployment:
-#    def __init__(self):
-#        self.classifier = pipeline('sentiment-analysis')
-#
-#    async def __call__(self, request):
-#        data = await request.body()
-#        [result] = self.classifier(str(data))
-#        return result['label']
-
-#class SentimentDeployment:
-#    def __init__(self):
-#        fs = s3fs.S3FileSystem(anon=True)
-#        with fs.open('ray-serve-blog/unigram_vectorizer.joblib', 'rb') as f:
-#            self.vectorizer = joblib.load(f)
-#        with fs.open('ray-serve-blog/unigram_tf_idf_transformer.joblib', 'rb') as f:
-#            self.preprocessor = joblib.load(f)
-#        with fs.open('ray-serve-blog/unigram_tf_idf_classifier.joblib', 'rb') as f:
-#            self.classifier = joblib.load(f)
-#
-#    async def __call__(self, request):
-#        data = await request.body()
-#        vectorized = self.vectorizer.transform([str(data)])
-#        transformed = self.preprocessor.transform(vectorized)
-#        [result] = self.classifier.predict(transformed)
-#        if result == 1:
-#            return 'POSITIVE'
-#        else:
-#            return 'NEGATIVE'
--- sustainable-deep-learning/scripts/model_client.py
+++ sustainable-deep-learning/scripts/model_client.py
@@ -1,8 +1,6 @@
 import requests
 
 
-#response = requests.post("http://127.0.0.1:8000/", params={"text": "Hello world!"})
 response = requests.post('http://127.0.0.1:8000/', params={'text': 'Hello world!'})
 french_text = response.json()
 print('french_text: ' + str(french_text))
-#print(response)
