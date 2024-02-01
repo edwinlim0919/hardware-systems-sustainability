370549c9ec779bd5954e1b77ff10f7009b285545
edwinlim <edwinlim@node0.sgrfcmu.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-01 12:46:54 -0500

    stuff

--- sustainable-deep-learning/scripts/model.py
+++ sustainable-deep-learning/scripts/model.py
@@ -1,17 +1,46 @@
+import ray
+from ray import serve
+from fastapi import FastAPI
+
 from transformers import pipeline
 #import joblib
 #import s3fs
 #import sklearn
 
-@serve.deployment(route_prefix='/sentiment', name='sentiment')
-class SentimentDeployment:
-    def __init__(self):
-        self.classifier = pipeline('sentiment-analysis')
 
-    async def __call__(self, request):
-        data = await request.body()
-        [result] = self.classifier(str(data))
-        return result['label']
+app = FastAPI()
+
+
+@serve.deployment(num_replicas=2, ray_actor_options={'num_cpus': 0.2, 'num_gpus': 0})
+@serve.ingress(app)
+class Translator:
+    def __init__(self):
+        self.model = pipeline('translation_en_to_fr', model='t5-small')
+
+    @app.post("/")
+    def translate(self, text: str) -> str:
+        model_output = self.model(text)
+        translation = model_output[0]['translation_text']
+        return translation
+
+
+#translator = Translator()
+#translation = translator.translate('Hello world!')
+#print(translation)
+
+
+translator_app = Translator.bind()
+
+
+#@serve.deployment(route_prefix='/sentiment', name='sentiment')
+#class SentimentDeployment:
+#    def __init__(self):
+#        self.classifier = pipeline('sentiment-analysis')
+#
+#    async def __call__(self, request):
+#        data = await request.body()
+#        [result] = self.classifier(str(data))
+#        return result['label']
 
 #class SentimentDeployment:
 #    def __init__(self):
