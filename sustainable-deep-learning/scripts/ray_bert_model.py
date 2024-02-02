28e6bc9b7dd063af1392b5368c221bf177e240aa
edwinlim <edwinlim@node0.sgrfcmu.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-02 17:10:53 -0500

    wip

--- sustainable-deep-learning/scripts/ray_bert_model.py
+++ sustainable-deep-learning/scripts/ray_bert_model.py
@@ -1,6 +1,8 @@
 import torch
 import ray
 from ray import serve
+#from ray.serve.drivers import DAGDriver
+from ray.serve.handle import DeploymentHandle
 from fastapi import FastAPI
 from transformers import BertModel, BertTokenizer
 from transformers import AutoTokenizer, BertForQuestionAnswering
@@ -11,7 +13,7 @@ app = FastAPI()
 
 # Base model inference for feature vector
 @serve.deployment(num_replicas=2, ray_actor_options={'num_cpus': 0.2, 'num_gpus': 0}) # TODO: change resource alloc
-@serve.ingress(app)
+#@serve.ingress(app)
 class BertCL4InferenceRay:
     def __init__(self):
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
@@ -20,8 +22,9 @@ class BertCL4InferenceRay:
         self.model.eval()
 
     # Returns a feature vector from mean of concatenated last 4 hidden states
-    @app.post('/bert/cl4inference')
-    def inference(self, text: str) -> str:
+    #@app.post('/bert/cl4inference')
+    #def inference(self, text: str) -> str:
+    def __call__(self, text: str) -> str:
         query_ids = self.tokenizer.encode(text, return_tensors='pt')
         query_ids = query_ids.to('cpu')
         with torch.no_grad():
@@ -35,14 +38,15 @@ class BertCL4InferenceRay:
 
 # BERT for question answering
 @serve.deployment(num_replicas=2, ray_actor_options={'num_cpus': 0.2, 'num_gpus': 0}) # TODO: change resource alloc
-@serve.ingress(app)
+#@serve.ingress(app)
 class BertQAInferenceRay:
     def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained("deepset/bert-base-cased-squad2")
         self.model = BertForQuestionAnswering.from_pretrained("deepset/bert-base-cased-squad2")
 
-    @app.post('/bert/qainference')
-    def inference(self, question: str, text: str) -> str:
+    #@app.post('/bert/qainference')
+    #def inference(self, question: str, text: str) -> str:
+    def __call__(self, question: str, text: str) -> str:
         inputs = self.tokenizer(question, text, return_tensors="pt")
         with torch.no_grad():
             outputs = self.model(**inputs)
@@ -53,18 +57,9 @@ class BertQAInferenceRay:
         return self.tokenizer.decode(predict_answer_tokens, skip_special_tokens=True)
 
 
-bert_cl4_inference_app = BertCL4InferenceRay.bind()
-bert_qa_inference_app = BertQAInferenceRay.bind()
+#bert_cl4_inference_app = BertCL4InferenceRay.bind()
+#bert_qa_inference_app = BertQAInferenceRay.bind()
 
 
-#BertCL4Inference = False
-#if BertCL4Inference:
-#    bertmodel = BertCL4InferenceRay()
-#    cat_sentence_embedding = bertmodel.inference('granola bars')
-#    print('cat_sentence_embedding: ' + str(cat_sentence_embedding))
-#    print('cat_sentence_embedding.size(): ' + str(cat_sentence_embedding.size()))
-#BertQAInference = True
-#if BertQAInference:
-#    bertmodel = BertQAInferenceRay()
-#    answer = bertmodel.inference("Who was Jim Henson?", "Jim Henson was a nice puppet")
-#    print(answer)
+d = DAGDriver.bind({'/bert/cl4inference': BertCL4InferenceRay.bind(), '/bert/qainference': BertQAInferenceRay})
+handle = serve.run(d)
