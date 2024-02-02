e41e4eabeb87b9f26e684a48f2124b6090c8e111
edwinlim <edwinlim@node0.sgrfcmu.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-02 14:57:20 -0500

    bert concat last 4 layer feature vector

--- sustainable-deep-learning/scripts/ray_bert-base-uncased_model.py
+++ sustainable-deep-learning/scripts/ray_bert-base-uncased_model.py
@@ -2,19 +2,30 @@ import torch
 from transformers import BertModel, BertTokenizer
 
 
-class BertModel:
+class BertModelRayCPU:
     def __init__(self):
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
+        self.model = self.model.to('cpu')
+        self.model.eval()
 
-    def tokenize(self, text: str) -> str:
+    # Returns a feature vector from mean of concatenated last 4 hidden states
+    def CL4_embedding_inference(self, text: str) -> str:
         query_ids = self.tokenizer.encode(text, return_tensors='pt')
-        #query_tokens = self.tokenizer.convert_ids_to_tokens(query_ids)
-        #query_ids = torch.LongTensor(query_ids)
-        print('query_ids: ' + str(query_ids))
-        print('type of query_ids' + str(type(query_ids)))
-        #print('query_tokens: ' + str(query_tokens))
-        return 'e'
+        query_ids = query_ids.to('cpu')
+        with torch.no_grad():
+            out = self.model(input_ids=query_ids)
+
+        hidden_states = out[2]
+        last_four_layers = [hidden_states[i] for i in (-1, -2, -3, -4)]
+        cat_hidden_states = torch.cat(tuple(last_four_layers), dim=-1)
+        return torch.mean(cat_hidden_states, dim=1).squeeze()
 
 
-bertmodel = BertModel()
-bertmodel.tokenize('granola bars')
+bertmodel = BertModelRayCPU()
+cat_sentence_embedding = bertmodel.CL4_embedding_inference('granola bars')
+
+
+print('cat_sentence_embedding: ' + str(cat_sentence_embedding))
+print('cat_sentence_embedding.size(): ' + str(cat_sentence_embedding.size()))
+
