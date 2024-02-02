305afead744b9186b0d3a6fb14f67bcb2cddc485
edwinlim <edwinlim@node0.sgrfcmu.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-02 17:53:20 -0500

    wip

--- sustainable-deep-learning/scripts/ray_bert_model.py
+++ sustainable-deep-learning/scripts/ray_bert_model.py
@@ -8,7 +8,7 @@ from transformers import BertModel, BertTokenizer
 from transformers import AutoTokenizer, BertForQuestionAnswering
 
 
-app = FastAPI()
+#app = FastAPI()
 
 
 # Base model inference for feature vector
@@ -24,7 +24,7 @@ class BertCL4InferenceRay:
     # Returns a feature vector from mean of concatenated last 4 hidden states
     #@app.post('/bert/cl4inference')
     #def inference(self, text: str) -> str:
-    def __call__(self, text: str) -> str:
+    async def __call__(self, text: str) -> str:
         query_ids = self.tokenizer.encode(text, return_tensors='pt')
         query_ids = query_ids.to('cpu')
         with torch.no_grad():
@@ -36,25 +36,25 @@ class BertCL4InferenceRay:
         return str(torch.mean(cat_hidden_states, dim=1).squeeze())
 
 
-# BERT for question answering
-@serve.deployment(num_replicas=2, ray_actor_options={'num_cpus': 0.2, 'num_gpus': 0}) # TODO: change resource alloc
-#@serve.ingress(app)
-class BertQAInferenceRay:
-    def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained("deepset/bert-base-cased-squad2")
-        self.model = BertForQuestionAnswering.from_pretrained("deepset/bert-base-cased-squad2")
-
-    #@app.post('/bert/qainference')
-    #def inference(self, question: str, text: str) -> str:
-    def __call__(self, question: str, text: str) -> str:
-        inputs = self.tokenizer(question, text, return_tensors="pt")
-        with torch.no_grad():
-            outputs = self.model(**inputs)
-
-        answer_start_index = outputs.start_logits.argmax()
-        answer_end_index = outputs.end_logits.argmax()
-        predict_answer_tokens = inputs.input_ids[0, answer_start_index : answer_end_index + 1]
-        return self.tokenizer.decode(predict_answer_tokens, skip_special_tokens=True)
+## BERT for question answering
+#@serve.deployment(num_replicas=2, ray_actor_options={'num_cpus': 0.2, 'num_gpus': 0}) # TODO: change resource alloc
+##@serve.ingress(app)
+#class BertQAInferenceRay:
+#    def __init__(self):
+#        self.tokenizer = AutoTokenizer.from_pretrained("deepset/bert-base-cased-squad2")
+#        self.model = BertForQuestionAnswering.from_pretrained("deepset/bert-base-cased-squad2")
+#
+#    #@app.post('/bert/qainference')
+#    #def inference(self, question: str, text: str) -> str:
+#    def __call__(self, question: str, text: str) -> str:
+#        inputs = self.tokenizer(question, text, return_tensors="pt")
+#        with torch.no_grad():
+#            outputs = self.model(**inputs)
+#
+#        answer_start_index = outputs.start_logits.argmax()
+#        answer_end_index = outputs.end_logits.argmax()
+#        predict_answer_tokens = inputs.input_ids[0, answer_start_index : answer_end_index + 1]
+#        return self.tokenizer.decode(predict_answer_tokens, skip_special_tokens=True)
 
 
 #bert_cl4_inference_app = BertCL4InferenceRay.bind()
