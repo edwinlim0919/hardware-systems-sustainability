b6585be40ebd3b5a7ccad3d03475eb4a5bb400b5
edwinlim <edwinlim@node0.sgrfcmu.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-03 23:02:38 -0500

    both ray endpoints working

--- sustainable-deep-learning/scripts/ray_bert_client.py
+++ sustainable-deep-learning/scripts/ray_bert_client.py
@@ -3,12 +3,20 @@ import requests
 
 cl4_response = requests.post(
     'http://127.0.0.1:8000/bert/cl4inference',
-    json={'inference_type': 'BertCL4', 'inference_text': 'granola bars'})
+    json={
+        'inference_type': 'BertCL4',
+        'inference_text': 'granola bars'
+    }
+)
 print('cl4_response.text: ' + str(cl4_response.text))
-#cl4_sentence_embedding = cl4_response.json()
-#print('cl4_sentence_embedding: ' + str(cl4_sentence_embedding))
 
 
-#qa_response = requests.post('http://127.0.0.1:8000/bert/qainference', params={'question': 'Who was Jim Henson?', 'text': "Jim Henson was a nice puppet"})
-#qa_answer = qa_response.json()
-#print('qa_answer: ' + str(qa_answer))
+qa_response = requests.post(
+    'http://127.0.0.1:8000/bert/qainference',
+    json={
+        'inference_type': 'BertQA',
+        'inference_question': 'Who was Jim Henson?',
+        'inference_text': "Jim Henson was a nice puppet"
+    }
+)
+print('qa_response.text: ' + str(qa_response.text))
