521ff75e91b45740fd2ff49d52137821c69afd94
edwinlim <edwinlim@node0.siafg.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-04 14:49:58 -0500

    auto

--- sustainable-deep-learning/scripts/ray_bert_model.py
+++ sustainable-deep-learning/scripts/ray_bert_model.py
@@ -34,7 +34,7 @@ class BertEndpointRay:
 @serve.deployment
 class BertCL4InferenceRay:
     def __init__(self):
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
         self.model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
         self.model = self.model.to('cpu')
         self.model.eval()
