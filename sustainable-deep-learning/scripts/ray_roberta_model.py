3ea0ec9723a8167e8e612f870d85ed53ae06186d
edwinlim <edwinlim@node0.siafg.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-04 15:20:38 -0500

    robert base inference

--- sustainable-deep-learning/scripts/ray_roberta_model.py
+++ sustainable-deep-learning/scripts/ray_roberta_model.py
@@ -3,7 +3,26 @@ from transformers import AutoTokenizer, RobertaModel
 
 
 # Base model inference for feature vector
-class RobertaCL4InferenceRay:
+class RobertaBaseInferenceRay:
     def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained('roberta-base')
-        self.model = 
+        self.model = RobertaModel.from_pretrained('roberta-base', output_hidden_states=True)
+
+    # Returns a feature vector from mean of concatenated last 4 hidden states
+    def inference(self, text: str) -> str:
+        query_ids = self.tokenizer.encode(text, return_tensors='pt')
+        with torch.no_grad():
+            outputs = self.model(input_ids=query_ids)
+
+        hidden_states = outputs[2]
+        last_four_layers = [hidden_states[i] for i in (-1, -2, -3, -4)]
+        cat_hidden_states = torch.cat(tuple(last_four_layers), dim=-1)
+        return str(torch.mean(cat_hidden_states, dim=1).squeeze())
+
+
+
+
+
+roberta_model = RobertaBaseInferenceRay()
+cl4_inf_result = roberta_model.inference('granola bars')
+print('cl4_inf_result: ' + cl4_inf_result)
