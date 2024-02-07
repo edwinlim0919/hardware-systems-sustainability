8cb5e976f18b3e424d93e24b6b1878294b990c73
edwinlim <edwinlim@node0.siafg.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-06 19:01:55 -0500

    e

--- sustainable-deep-learning/infra_scripts/scp_model_weights.sh
+++ sustainable-deep-learning/infra_scripts/scp_model_weights.sh
@@ -4,17 +4,17 @@
 #
 # Arguments
 #   args[0] : <user>@<address> for scp/ssh to remote machine
-#   args[1] : absolute path to weights folder destination in remote machine
-#   args[2] : name of first weights folder
+#   args[1] : absolute path to weights file/folder destination in remote machine
+#   args[2] : name of first weights file/folder
 #   ...
-#   args[n] : name of last weights folder
+#   args[n] : name of last weights filefolder
 #
 # Example Usage
-#   ./scp_model_weights.sh edwinlim@clnode219.clemson.cloudlab.us /dev/shm/hardware-systems-sustainability/sustainable-deep-learning/model_weights llama-2-7b-chat llama-2-13b-chat llama-2-70b-chat
+#   ./scp_model_weights.sh edwinlim@clnode219.clemson.cloudlab.us /dev/shm/hardware-systems-sustainability/sustainable-deep-learning/model_weights llama-2-7b-chat llama-2-13b-chat llama-2-70b-chat tokenizer.model tokenizer_checklist.chk
 
 
 if [ "$#" -lt 3 ]; then
-    echo "Usage: $0 <user>@<address> <destination_path> <folder1> ... <folderN>"
+    echo "Usage: $0 <user>@<address> <destination_path> <f1> ... <fN>"
     exit 1
 fi
 
@@ -25,7 +25,7 @@ echo "Remote user@host: $remote_user_host"
 echo "Destination path: $destination_path"
 
 
-for folder in "${@:3}"; do
-  echo $folder
-  scp -r ${folder} ${remote_user_host}:${destination_path}/${folder}
+for f in "${@:3}"; do
+  echo $f
+  scp -r ${f} ${remote_user_host}:${destination_path}/${f}
 done
