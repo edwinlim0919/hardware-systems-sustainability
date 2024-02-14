a3d6f9e7fa6e1c6d655ade50eac26d0a2b526c27
edwinlim <edwinlim@node0.siafg.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-14 15:18:21 -0500

    wip

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-curl -fsSL https://get.docker.com -o scripts/get-docker.sh
-sudo sh scripts/get-docker.sh
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+#curl -fsSL https://get.docker.com -o scripts/get-docker.sh
+#sudo sh scripts/get-docker.sh
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
+#sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 sudo apt-get install libssl-dev -y
 sudo apt-get install libz-dev -y
@@ -12,3 +12,6 @@ sudo apt-get install luarocks -y
 sudo luarocks install luasocket
 sudo apt install linux-tools-`uname -r` linux-tools-generic htop -y
 sudo apt install libelf-dev libdw-dev systemtap-sdt-dev libunwind-dev libslang2-dev libnuma-dev libiberty-dev -y
+
+sudo apt-get update
+sudo apt-get install -y cmake
