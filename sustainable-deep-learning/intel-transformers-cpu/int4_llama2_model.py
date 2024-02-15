d232daf905a27a9bc4d23af89068fc9958809aa6
edwinlim <edwinlim@node0.siafg.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-15 14:48:03 -0500

    intel cry intel cry

--- sustainable-deep-learning/intel-transformers-cpu/int4_llama2_model.py
+++ sustainable-deep-learning/intel-transformers-cpu/int4_llama2_model.py
@@ -1,13 +1,36 @@
+import transformers
 from transformers import AutoTokenizer, TextStreamer
 from intel_extension_for_transformers.transformers import AutoModelForCausalLM
+from intel_extension_for_transformers.transformers.pipeline import pipeline
+
 
 model_name = 'TheBloke/Llama-2-7B-Chat-GGUF'
 model_file = 'llama-2-7b-chat.Q4_0.gguf'
 tokenizer_name = 'meta-llama/Llama-2-7b-chat-hf'
+model_config = transformers.AutoConfig.from_pretrained(model_name)
 
 prompt = 'Once upon a time'
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, trust_remote_code=True)
 inputs = tokenizer(prompt, return_tensors="pt").input_ids
-streamer = TextStreamer(tokenizer)
-model = AutoModelForCausalLM.from_pretrained(model_name, model_file = model_file)
-outputs = model.generate(inputs, streamer=streamer, max_new_tokens=300)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    model_file=model_file,
+    trust_remote_code=True
+    #config=model_config
+)
+
+
+#streamer = TextStreamer(tokenizer)
+#outputs = model.generate(inputs, streamer=streamer, max_new_tokens=300)
+
+#generate_text = pipeline(
+#    model=model,
+#    config=model_config,
+#    tokenizer=tokenizer,
+#    return_full_text=True,
+#    task='text-generation',
+#    temperature=0.0,
+#    max_new_tokens=512,
+#    repetition_penalty=1.1
+#)
