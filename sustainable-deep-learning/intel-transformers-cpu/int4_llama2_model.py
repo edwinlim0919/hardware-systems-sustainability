c0b601e9e11431f32b1a9b4e51e66af0350512f3
edwinlim <edwinlim@node0.siafg.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-17 16:15:44 -0500

    cringe

--- sustainable-deep-learning/intel-transformers-cpu/int4_llama2_model.py
+++ sustainable-deep-learning/intel-transformers-cpu/int4_llama2_model.py
@@ -8,20 +8,28 @@ from intel_extension_for_transformers.transformers import AutoModelForCausalLM
 model_name = 'TheBloke/Llama-2-7B-Chat-GGUF'
 model_file = 'llama-2-7b-chat.Q4_0.gguf'
 tokenizer_name = 'meta-llama/Llama-2-7b-chat-hf'
-#model_config = transformers.AutoConfig.from_pretrained(model_name, model_file=model_file)
 
-print('model_config: ' + str(model_config))
-
-prompt = 'Once upon a time'
+prompt = "Once upon a time"
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, trust_remote_code=True)
 inputs = tokenizer(prompt, return_tensors="pt").input_ids
+streamer = TextStreamer(tokenizer)
+model = AutoModelForCausalLM.from_pretrained(model_name, model_file = model_file)
+outputs = model.generate(inputs, streamer=streamer, max_new_tokens=300)
 
-model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    model_file=model_file,
-    trust_remote_code=True,
-    config=model_config
-)
+#model_config = transformers.AutoConfig.from_pretrained(model_name, model_file=model_file)
+
+#print('model_config: ' + str(model_config))
+
+#prompt = 'Once upon a time'
+#tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, trust_remote_code=True)
+#inputs = tokenizer(prompt, return_tensors="pt").input_ids
+#
+#model = AutoModelForCausalLM.from_pretrained(
+#    model_name,
+#    model_file=model_file,
+#    trust_remote_code=True,
+#    config=model_config
+#)
 
 
 #text_classifier = pipeline(
