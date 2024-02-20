b77461c130dfedb949ab7fad43bb64c8fe21d75b
edwinlim <edwinlim@node0.siafg.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-20 11:33:48 -0500

    pissin and shittin

--- sustainable-deep-learning/intel-transformers-cpu/int4_llama2_model.py
+++ sustainable-deep-learning/intel-transformers-cpu/int4_llama2_model.py
@@ -1,73 +1,49 @@
-#import transformers
 from transformers import AutoTokenizer, TextStreamer
 from intel_extension_for_transformers.transformers import AutoModelForCausalLM
-#from intel_extension_for_transformers.transformers.pipeline import pipeline
-#from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 
 
-model_name = 'TheBloke/Llama-2-7B-Chat-GGUF'
-model_file = 'llama-2-7b-chat.Q4_0.gguf'
-tokenizer_name = 'meta-llama/Llama-2-7b-chat-hf'
+class Llama2Int4InferenceRay:
+    def __init__(self):
+        model_name = 'TheBloke/Llama-2-7B-Chat-GGUF'
+        model_file = 'llama-2-7b-chat.Q4_0.gguf'
+        tokenizer_name = 'meta-llama/Llama-2-7b-chat-hf'
 
-prompt = "Once upon a time"
-tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, trust_remote_code=True)
-inputs = tokenizer(prompt, return_tensors="pt").input_ids
-streamer = TextStreamer(tokenizer)
-model = AutoModelForCausalLM.from_pretrained(model_name, model_file = model_file)
-outputs = model.generate(inputs, streamer=streamer, max_new_tokens=300)
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            tokenizer_name,
+            trust_remote_code=True
+        )
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            model_file=model_file
+        )
 
-#model_config = transformers.AutoConfig.from_pretrained(model_name, model_file=model_file)
+    def inference(self, prompt: str) -> str:
+        inputs = self.tokenizer(
+            prompt,
+            return_tensors='pt'
+        ).input_ids
+        outputs = self.model.generate(
+            inputs,
+            max_new_tokens=512
+        )
+        response = self.tokenizer.decode(
+            outputs[0],
+            skip_special_tokens=True
+        )
+        print(response)
+        return response
 
-#print('model_config: ' + str(model_config))
 
-#prompt = 'Once upon a time'
+llama2_int4_inference = Llama2Int4InferenceRay()
+response = llama2_int4_inference.inference('Once upon a time')
+
+#model_name = 'TheBloke/Llama-2-7B-Chat-GGUF'
+#model_file = 'llama-2-7b-chat.Q4_0.gguf'
+#tokenizer_name = 'meta-llama/Llama-2-7b-chat-hf'
+#
+#prompt = "Once upon a time"
 #tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, trust_remote_code=True)
 #inputs = tokenizer(prompt, return_tensors="pt").input_ids
-#
-#model = AutoModelForCausalLM.from_pretrained(
-#    model_name,
-#    model_file=model_file,
-#    trust_remote_code=True,
-#    config=model_config
-#)
-
-
-#text_classifier = pipeline(
-#    task='text-classification',
-#    model='TheBloke/Llama-2-7B-Chat-GGUF',
-#    model_file='llama-2-7b-chat.Q4_0.gguf'
-#)
-
-
-#pipe = HuggingFacePipeline(pipeline=pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=128))
-
 #streamer = TextStreamer(tokenizer)
+#model = AutoModelForCausalLM.from_pretrained(model_name, model_file = model_file)
 #outputs = model.generate(inputs, streamer=streamer, max_new_tokens=300)
-
-
-#model = transformers.AutoModelForCausalLM.from_pretrained(
-#    model_name,
-#    model_file=model_file,
-#    trust_remote_code=True,
-#    config=model_config
-#)
-
-
-#generate_text = pipeline(
-#    model=model,
-#    config=model_config,
-#    tokenizer=tokenizer,
-#    return_full_text=True,
-#    task='text-generation',
-#    temperature=0.0,
-#    max_new_tokens=512,
-#    repetition_penalty=1.1
-#)
-
-#pipe = HuggingFacePipeline(pipeline=pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=128))
-#retriever = VectorStoreRetriever(vectorstore=knowledge_base, search_type='mmr', search_kwargs={'k':1, 'fetch_k':5})
-#retrievalQA = RetrievalQA.from_llm(llm=pipe, retriever=retriever)
-#
-#generate_text = pipeline(
-#    model=
-#)
