9b2651682d9e2c229819fa2242caf54c1bfa77e0
edwinlim <edwinlim@node0.siafg.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-20 10:50:05 -0500

    setup

--- sustainable-deep-learning/intel-transformers-cpu/setup.sh
+++ sustainable-deep-learning/intel-transformers-cpu/setup.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
+conda create --name intel-transformers python=3.11
+conda activate intel-transformers
+conda install pip
+export PATH='/users/'"${USER}"'/miniconda3/envs/intel-transformers/bin:'"$PATH"
+#conda deactivate
+
+
 python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 python -m pip install intel-extension-for-pytorch
 python -m pip install oneccl_bind_pt --extra-index-url https://pytorch-extension.intel.com/release-whl/stable/cpu/us/
--- sustainable-deep-learning/setup.sh
+++ sustainable-deep-learning/setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-#pip install -r requirements.txt
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
@@ -9,26 +8,10 @@ rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
 
 
-#conda create --name neural-speed-env
-#conda activate neural-speed-env
-#export PATH='/users/'"${USER}"'/miniconda3/envs/neural-speed-env/bin:'"$PATH"
+#conda create --name intel-transformers python=3.11
+#conda activate intel-transformers
 #conda install pip
-#pip install -r neural-speed/requirements.txt
-#pip install ./neural-speed/
+#export PATH='/users/'"${USER}"'/miniconda3/envs/intel-transformers/bin:'"$PATH"
 #conda deactivate
-#
-#conda create --name llama-cpp
-#conda activate llama-cpp
-#export PATH='/users/'"${USER}"'/miniconda3/envs/llama-cpp/bin:'"$PATH"
-#conda install pip
-#pip install -U pyllama transformers
-#pip install -r requirements.txt
-#conda deactivate
-
-conda create --name intel-transformers python=3.11
-conda activate intel-transformers
-conda install pip
-export PATH='/users/'"${USER}"'/miniconda3/envs/intel-transformers/bin:'"$PATH"
-#pip install -r neural-speed/requirements.txt
-#pip install ./neural-speed/
-conda deactivate
+git submodule init neural-speed
+git submodule update neural-speed
