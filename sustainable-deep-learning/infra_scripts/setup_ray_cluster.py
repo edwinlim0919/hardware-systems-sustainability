531890601539a5aa08112e188f9ab3cadd4bc307
edwinlim <edwinlim@node0.siafg.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-23 10:48:45 -0500

    remote worker setup finally working

--- sustainable-deep-learning/infra_scripts/setup_ray_cluster.py
+++ sustainable-deep-learning/infra_scripts/setup_ray_cluster.py
@@ -58,8 +58,30 @@ def start_head_node():
     return execute_cmd(start_cmd)
 
 
+# because I can't figure out ssh forwarding...
+def ssh_and_run_command(host, command):
+    ssh_command = f"ssh -A {host} {command}"
+    try:
+        result = subprocess.run(
+            ssh_command,
+            shell=True,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command: {command}")
+
+
 def setup_worker_node(username, host):
     try:
+        # ugh
+        remote_host = f'{username}@{host}'
+        rm_command = 'rm -rf neural-speed'
+        clone_command = 'git clone git@github.com:edwinlim0919/neural-speed.git'
+        ssh_and_run_command(remote_host, rm_command)
+        ssh_and_run_command(remote_host, clone_command)
+
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(host, username=username)
@@ -73,15 +95,33 @@ def setup_worker_node(username, host):
             local_script_path,
             remote_script_path
         )
-        local_env_path = f'{curr_dir}/environment.yaml'
-        remote_env_path = f'/users/{username}/environment.yaml'
+        key_path = f'/users/{username}/.ssh/id_rsa'
         sftp.put(
-            local_env_path,
-            remote_env_path
+            key_path,
+            key_path
+        )
+        config_path = f'/users/{username}/.ssh/config'
+        sftp.put(
+            config_path,
+            config_path
+        )
+        #local_env_path = f'{curr_dir}/environment.yaml'
+        #remote_env_path = f'/users/{username}/environment.yaml'
+        #sftp.put(
+        #    local_env_path,
+        #    remote_env_path
+        #)
+        local_reqs_path = f'{curr_dir}/../intel-transformers-cpu/requirements.txt'
+        remote_reqs_path = f'/users/{username}/requirements.txt'
+        sftp.put(
+            local_reqs_path,
+            remote_reqs_path
         )
         sftp.close()
+        ssh.close()
 
         # setup base dependencies
+        ssh.connect(host, username=username)
         command = f'chmod +x {remote_script_path} && yes | {remote_script_path}'
         stdin, stdout, stderr = ssh.exec_command(command)
 
@@ -94,19 +134,19 @@ def setup_worker_node(username, host):
             print(stderr.read().decode())
 
         # setup conda env
-        combined_commands = (
-            'export PATH="$HOME/miniconda3/bin:$PATH" && '
-            'eval "$(conda shell.bash hook)" && '
-            f'conda env create -f {remote_env_path} -n worker-env && '
-            'conda activate worker-env'
-        )
-        stdin, stdout, stderr = ssh.exec_command(combined_commands)
-        output = stdout.read().decode()
-        error = stderr.read().decode()
-        if output:
-            print(output)
-        if error:
-            print(error)
+        #combined_commands = (
+        #    'export PATH="$HOME/miniconda3/bin:$PATH" && '
+        #    'eval "$(conda shell.bash hook)" && '
+        #    f'conda env create -f {remote_env_path} -n worker-env && '
+        #    'conda activate worker-env'
+        #)
+        #stdin, stdout, stderr = ssh.exec_command(combined_commands)
+        #output = stdout.read().decode()
+        #error = stderr.read().decode()
+        #if output:
+        #    print(output)
+        #if error:
+        #    print(error)
 
         #commands = [
         #    'export PATH="$HOME/miniconda3/bin:$PATH"',
@@ -144,13 +184,12 @@ if __name__ == '__main__':
         required=True,
         help='The filename containing SSH commands.'
     )
-    parser.add_argument(
-        '--conda-env-yaml',
-        required=True,
-        help='Name of .yaml describing your conda environment.'
-    )
+    #parser.add_argument(
+    #    '--conda-env-yaml',
+    #    required=True,
+    #    help='Name of .yaml describing your conda environment.'
+    #)
     args = parser.parse_args()
-
     setup_worker_nodes(args.ssh_list)
 
     #worker_connect_command = start_head_node()
--- sustainable-deep-learning/infra_scripts/worker_setup.sh
+++ sustainable-deep-learning/infra_scripts/worker_setup.sh
@@ -1,22 +1,33 @@
 #!/usr/bin/env bash
 
-sudo apt-get install libssl-dev -y
-sudo apt-get install libz-dev -y
-sudo apt-get install luarocks -y
-sudo luarocks install luasocket
-sudo apt install linux-tools-`uname -r` linux-tools-generic htop -y
-sudo apt install libelf-dev libdw-dev systemtap-sdt-dev libunwind-dev libslang2-dev libnuma-dev libiberty-dev -y
+#sudo apt-get install libssl-dev -y
+#sudo apt-get install libz-dev -y
+#sudo apt-get install luarocks -y
+#sudo luarocks install luasocket
+#sudo apt install linux-tools-`uname -r` linux-tools-generic htop -y
+#sudo apt install libelf-dev libdw-dev systemtap-sdt-dev libunwind-dev libslang2-dev libnuma-dev libiberty-dev -y
+#sudo apt-get update
+#sudo apt-get install -y cmake
+#sudo apt install docker.io
+#
+#
+#mkdir -p ~/miniconda3
+#wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+#bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+#rm -rf ~/miniconda3/miniconda.sh
+#~/miniconda3/bin/conda init bash
+export PATH="$HOME/miniconda3/bin:$PATH"
 
-sudo apt-get update
-sudo apt-get install -y cmake
+conda init
+conda create --name intel-transformers python=3.11
+conda activate intel-transformers
+conda install pip
+export PATH='/users/'"${USER}"'/miniconda3/envs/intel-transformers/bin:'"$PATH"
 
+python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+python -m pip install intel-extension-for-pytorch
+python -m pip install oneccl_bind_pt --extra-index-url https://pytorch-extension.intel.com/release-whl/stable/cpu/us/
+pip install -r requirements.txt
 
-mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -rf ~/miniconda3/miniconda.sh
-
-~/miniconda3/bin/conda init bash
-
-
-sudo apt install docker.io
+pip install -r neural-speed/requirements.txt
+pip install ./neural-speed/
