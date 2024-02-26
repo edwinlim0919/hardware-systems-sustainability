49ada9f0a516af5c8c59a58142e985c39cac7c03
edwinlim <edwinlim@node0.siafg.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-25 22:17:51 -0500

    wkrgen wip

--- sustainable-deep-learning/workload_generators/llama2_throughput_wrkgen.py
+++ sustainable-deep-learning/workload_generators/llama2_throughput_wrkgen.py
@@ -1,9 +1,13 @@
 import json
 import random
+import argparse
 
 
 # Sampling dataset prompts for throughput experiments
-def sample_dataset_prompts(dataset_path, num_requests):
+def sample_dataset_prompts(
+    dataset_path: str,
+    num_requests: int
+):
     with open(dataset_path, 'r', encoding='utf-8') as f:
         dataset = json.load(f)
 
@@ -22,16 +26,25 @@ def sample_dataset_prompts(dataset_path, num_requests):
 
 
 #shareGPT_file_path = 'ShareGPT_V3_unfiltered_cleaned_split.json'
-shareGPT_dataset_path = 'ShareGPT_V3_unfiltered_cleaned_split_top100.json'
-sample_dataset_prompts(shareGPT_dataset_path, 0)
+#shareGPT_dataset_path = 'ShareGPT_V3_unfiltered_cleaned_split_top100.json'
+#sample_dataset_prompts(shareGPT_dataset_path, 0)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='')
+    parser = argparse.ArgumentParser(description='Throughput generation script for LLM inference experiments.')
     parser.add_argument(
-        '--ssh-list',
+        '--dataset-path',
         required=True,
-        help='The filename containing SSH commands.'
+        help='The path to the dataset file.'
+    )
+    parser.add_argument(
+        '--num-requests',
+        required=True,
+        help='The number of requests to sample. Specify 0 or less to sample the entire dataset.'
     )
     args = parser.parse_args()
-    setup_worker_nodes(args.ssh_list)
+
+    sampled_dataset = sample_dataset_prompts(
+        args.dataset_path,
+        int(args.num_requests)
+    )
