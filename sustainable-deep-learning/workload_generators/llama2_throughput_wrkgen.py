3cc0639647ab90aa760ebb660dd240039bb8fe77
edwinlim <edwinlim@node0.siafg.datacenter-soc-pg0.clemson.cloudlab.us>
2024-02-26 12:19:14 -0500

    wip

--- sustainable-deep-learning/workload_generators/llama2_throughput_wrkgen.py
+++ sustainable-deep-learning/workload_generators/llama2_throughput_wrkgen.py
@@ -1,6 +1,9 @@
 import json
 import random
 import argparse
+import requests
+import numpy as np
+import time
 
 
 # Sampling dataset prompts for throughput experiments
@@ -23,6 +26,29 @@ def sample_dataset_prompts(
     print(sampled_dataset)
 
     return sampled_dataset
+
+
+# Generate a slowly increasing amount of request rates according to a Poisson distribution
+def generate_requests(
+    sampled_dataset: List[str],
+    requests_per_rate: int,         # requests
+    start_rate: int,                # requests per minute
+    end_rate: int,                  # requests per minute
+    rate_increase: int              # requests per minute
+):
+    # for reproducability
+    np.random.seed(42)
+    curr_rate = start_rate
+
+    while curr_rate < end_rate:
+        # requests per minute converted to requests per second
+        lambda_rate = curr_rate / 60
+
+        # calculating arrival times
+        inter_arrival_times = np.random.exponential(1 / lambda_rate, size=requests_per_rate)
+        arrival_times = np.cumsum(inter_arrival_times)
+
+        curr_rate += rate_increase
 
 
 #shareGPT_file_path = 'ShareGPT_V3_unfiltered_cleaned_split.json'
